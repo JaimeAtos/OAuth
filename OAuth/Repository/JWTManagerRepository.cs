0a778b90e91f62525ab0e0d287d8763131a9cd1d
Jaime Fernandez <luis-jaime.fernandez@atos.net>
2022-08-23 16:34:59 -0500

    Mejora del OAuth

--- OAuth/Repository/JWTManagerRepository.cs
+++ OAuth/Repository/JWTManagerRepository.cs
@@ -13,11 +13,11 @@ namespace OAuth.Repository
     public class JWTManagerRepository : IJWTManagerRepository
     {
         private readonly IConfiguration _configuration;
-        Dictionary<string, string> UserRecords = new Dictionary<string, string>
+        private readonly List<(string name, string password, string role)> UserRecords = new()
         {
-            {"user1", "password1" },
-            {"user2", "password2" },
-            {"user3", "password3" }
+            ("user1", "password1", "admin"),
+            ("user2", "password2", "guest" ),
+            ("user3", "password3", "hacker")
         };
 
         public JWTManagerRepository(IConfiguration configuration)
@@ -27,7 +27,7 @@ namespace OAuth.Repository
 
 		public Tokens Authenticate(Users users)
 		{
-			if (!UserRecords.Any(x => x.Key == users.Name && x.Value == users.Password))
+			if (!UserRecords.Any(x => x.name == users.Name && x.password == users.Password))
 			{
 				return null;
 			}
@@ -35,17 +35,22 @@ namespace OAuth.Repository
 			// Else we generate JSON Web Token
 			var tokenHandler = new JwtSecurityTokenHandler();
 			var tokenKey = Encoding.UTF8.GetBytes(_configuration["JWT:Key"]);
+			List<Claim> claims = new();
+			foreach (var role in users.Roles)
+			{
+				claims.Add(new Claim(ClaimTypes.Role, role));
+			}
+			claims.Add(new Claim(ClaimTypes.Name, users.Name));
 			var tokenDescriptor = new SecurityTokenDescriptor
 			{
-				Subject = new ClaimsIdentity(new Claim[]
-			  {
-			 new Claim(ClaimTypes.Name, users.Name)
-			  }),
-				Issuer = _configuration["JWT:Issuer"],
+
+                Subject = new ClaimsIdentity(claims.ToArray()),
+                Issuer = _configuration["JWT:Issuer"],
 				Audience = _configuration["JWT:Audience"],
 				Expires = DateTime.UtcNow.AddMinutes(10),
 				SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
 			};
+			
 			var token = tokenHandler.CreateToken(tokenDescriptor);
 			return new Tokens { Token = tokenHandler.WriteToken(token) };
 
